407c632a5e36a5318785818d7a4965f4ee35c8c2
Luís Bettencourt <lmn.bettencourt@gmail.com>
2021-02-20 18:47:01 +0000

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const colyseus = require("colyseus");
 const http = require("http");
 const express = require("express");
+var cors = require("cors");
 const {MyRoom} = require("./room");
 const port = process.env.port || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const gameServer = new colyseus.Server({
